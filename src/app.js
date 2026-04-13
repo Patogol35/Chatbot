471a02be4458608c07224fa52da8d4ab002ef952
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-13 13:11:30 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend PRO funcionando 🚀");
+  res.send("Backend funcionando");
 });
 
 app.post("/chat", chatHandler);
